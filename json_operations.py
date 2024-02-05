3d6e9864bb37927efd82b2365024eeb8bf0e7e72
Nischay <nischay26.scs@gmail.com>
2024-02-05 18:52:26 +0530

    updated README.md

--- json_operations.py
+++ json_operations.py
@@ -10,7 +10,7 @@ def generate_alert(template_path, projects, services):
     """
     This function receives the path of the template file and the data to be inserted in the alert.
     """
-    billing_dashboard_url = os.environ.get('billingDashboardUrl')
+    billing_dashboard_url = os.environ.get('BILLING_DASHBOARD_URL')
     with open(template_path, "r", encoding="utf-8") as read_file:
         template = json.load(read_file)
 
@@ -34,7 +34,7 @@ def send_alert(teams_alert):
     This function receives the alert and sends it to Microsoft Teams.
     """
     headers = {"Content-Type": "application/json"}
-    # webhook_url = os.environ.get('webhookURL')
+    # webhook_url = os.environ.get('WEBHOOK_URL')
 
     webhook_url = "https://whizlabsindia.webhook.office.com/webhookb2/14b2cc4e-2537-4ecc-a251-b2a43cb5953a@a34de549-3862-4aa2-94c8-7dfd72abb0a8/IncomingWebhook/992d4871d3a84a4aaccea22c32e6186f/c6a9b98f-9db4-4ef2-b206-29980d5c7dac"
 
